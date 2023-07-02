7accdcf5a0d52e2e13958c0521e8a6713042299d
Joa <91098938+joro-afk@users.noreply.github.com>
2023-07-03 01:10:52 +0200

    Update BaseHome.vue

--- src/components/share-tools/BaseHome.vue
+++ src/components/share-tools/BaseHome.vue
@@ -4,13 +4,13 @@
       <router-link to="/films">
         <div class="presentation-container-2">
           <h1 class="presentation-title">Films</h1>
-          <img src="@/assets/cinema.png" alt="" class="presentation-img" />
+          <img src="@/assets/images/cinema.png" alt="" class="presentation-img" />
         </div>
       </router-link>
       <router-link to="/tvshows">
         <div class="presentation-container-2">
           <h1 class="presentation-title">TV Shows</h1>
-          <img src="@/assets/tv.png" alt="" class="presentation-img" />
+          <img src="@/assets/images/tv.png" alt="" class="presentation-img" />
         </div>
       </router-link>
     </div>
