868eabf1d77dfcd41e15109824013ee7b37dfac5
Joa <91098938+joro-afk@users.noreply.github.com>
2023-07-03 01:11:22 +0200

    Update BaseTvShows.vue

--- src/components/share-tools/BaseTvShows.vue
+++ src/components/share-tools/BaseTvShows.vue
@@ -4,7 +4,7 @@
       <ul class="menu-container">
         <router-link to="/">
           <li class="menu home">
-            <img src="@/assets/home.png" class="home-img" alt="" />
+            <img src="@/assets/images/home.png" class="home-img" alt="" />
           </li>
         </router-link>
         <li class="menu" @click="tvshows.setTvShows()">On the air Tv shows</li>
@@ -13,7 +13,7 @@
         </li>
         <router-link to="/cart">
           <li class="menu cart">
-            <img src="@/assets/cart.png" class="home-img" alt="" />
+            <img src="@/assets/images/cart.png" class="home-img" alt="" />
             <p class="cart-amount">{{ tvshows.items.length }}</p>
           </li>
         </router-link>
@@ -67,7 +67,7 @@
                     />
                     <label for="cb1"
                       ><img
-                        src="@/assets/480-pixels.png"
+                        src="@/assets/images/480-pixels.png"
                         class="quality"
                         alt=""
                     /></label>
@@ -82,7 +82,7 @@
                     />
                     <label for="cb2">
                       <img
-                        src="@/assets/high-quality.png"
+                        src="@/assets/images/high-quality.png"
                         class="quality"
                         alt=""
                     /></label>
@@ -96,7 +96,7 @@
                       id="cb3"
                     />
                     <label for="cb3">
-                      <img src="@/assets/1080.png" class="quality" alt=""
+                      <img src="@/assets/images/1080.png" class="quality" alt=""
                     /></label>
                   </li>
                   <li class="purchate-li">
@@ -108,7 +108,7 @@
                       id="cb4"
                     />
                     <label for="cb4">
-                      <img src="@/assets/4k.png" class="quality" alt=""
+                      <img src="@/assets/images/4k.png" class="quality" alt=""
                     /></label>
                   </li>
                 </ul>
